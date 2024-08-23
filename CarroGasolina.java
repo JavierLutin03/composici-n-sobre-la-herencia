7d57874c82bbb0e1a99560d7084c4526787ce33a
JavierLutin03 <143854152+JavierLutin03@users.noreply.github.com>
2024-08-22 20:28:34 -0600

    Create CarroGasolina.java

---
+++ CarroGasolina.java
@@ -0,0 +1,16 @@
+package carro;
+
+public class CarroGasolina extends Carro {
+
+	public void  llenarTanque() {
+      System.out.println("Llenamos el tanque "); 
+	
+	}
+	
+
+      public CarroGasolina(String marc, String mod){
+    	  
+    	  super(marc, mod); 
+    	  
+      }
+}
