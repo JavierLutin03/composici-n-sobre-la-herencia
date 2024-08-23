14f06270e4d147c9960c781f36ce2e3f0b945271
JavierLutin03 <143854152+JavierLutin03@users.noreply.github.com>
2024-08-22 20:29:16 -0600

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,17 @@
+package carro;
+
+
+public class Carro {
+
+	string marca ; 
+	string modelo; 
+	
+	public Carro(string marc, string mod) {
+		
+		this.marca = marc; 
+		this.modelo = mod; 
+		
+		
+	}
+	
+}
