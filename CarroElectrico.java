e6114e57ac5939bfd11f987439ee338690be8515
JavierLutin03 <143854152+JavierLutin03@users.noreply.github.com>
2024-08-22 20:30:08 -0600

    Create CarroElectrico.java

---
+++ CarroElectrico.java
@@ -0,0 +1,20 @@
+package carro;
+
+public class CarroElectrico extends {
+
+	
+	
+	public CarroElectrico(String marc, String mod) {
+		
+		super(marc, mod);
+		
+	}
+		public void cargarBateria(){
+	          
+			
+		System.out.println("Hemos cargado la bateria "); 
+		
+	}
+
+
+}
